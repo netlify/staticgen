d1b3615d864f75ad62e831d0180607d51969056b
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-05-16 19:52:53 -0700

    Icons and color coding for trends on index page

--- config.rb
+++ config.rb
@@ -6,6 +6,16 @@ set :images_dir, 'images'
 
 activate :github
 
+helpers do
+  def caret_icon(number)
+    if number > 0
+      "<i class='fa fa-caret-up'></i>"
+    elsif number < 0
+      "<i class='fa fa-caret-down'></i>"
+    end
+  end
+end
+
 # Build-specific configuration
 configure :build do
 end
