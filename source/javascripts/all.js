1bf7d6542e258435a20838056b2188be64dfadde
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-05-19 10:48:13 -0700

    Update facebook app id

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -10,7 +10,7 @@ $(function() {
   var share = new Share("#share-button-top", {
     networks: {
       facebook: {
-        app_id: "200661259956061",
+        app_id: "1604147083144211",
       }
     }
   });
