a9cb469ae7f9cbaff6a8289c002ae429f2d9936a
Matthew Somerville <matthew-github@dracos.co.uk>
2018-01-15 15:05:53 +0000

    Escape special characters for language filtering.
    No need to treat '.net' specially any more.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -39,7 +39,7 @@ $(function() {
 
   $("select[name='filter']").change(function(e) {
     console.log("Filter by: %o", $(this).val());
-    $(".projects").isotope({filter: $(this).val().replace(/^\.lang-\./, '.lang-')});
+    $(".projects").isotope({filter: '.' + $(this).val().replace(/[^A-Z0-9_-]/gi, '\\$&')});
   });
 
   $("select[name='sort']").change(function(e) {
