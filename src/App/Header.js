84a36b319086c7771c8d7fa3c296ab64ecec0c90
Shawn Erquhart <shawn@erquh.art>
2018-11-05 13:43:34 -0500

    remove temp jamstack banner, preserve component

--- src/App/Header.js
+++ src/App/Header.js
@@ -5,43 +5,12 @@ import Hero from './Hero'
 import Nav from './Nav'
 import NavLink, { NavAnchor } from './NavLink'
 
-// swyx: temporary insert jamstack conf banner
-const JamstackConfBanner = ({ className }) => (
-  <div className={className}>
-    <p>
-      Learn more about the JAMstack at{' '}
-      <a href="https://jamstackconf.com">JAMstack Conf</a>, San Francisco —
-      29–30 October, 2018
-    </p>
-  </div>
-)
-const JamstackConfBannerStyled = styled(JamstackConfBanner)`
-  background-color: #000;
-  color: #fff;
-  display: block;
-  text-align: center;
-  z-index: 200;
-  position: fixed;
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  p {
-    margin: 0;
-    padding-top: 0.6em;
-    padding-bottom: 0.6em;
-  }
-`
-// swyx: temporary insert jamstack conf banner
-
 const Header = () => (
   <SiteData
     render={({
  title, subtitle, shareButtons, repo, pages, navLinks,
 }) => (
   <div>
-    {/* swyx: temporary insert jamstack conf banner */}
-    <JamstackConfBannerStyled />
-    {/* swyx: temporary insert jamstack conf banner */}
     <Hero
       title={title}
       subheading={subtitle}
