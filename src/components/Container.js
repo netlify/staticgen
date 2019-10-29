fae92f5db7695f7478e1dd050587126652637219
SeanRParker <seanrichardparker@gmail.com>
2019-10-29 10:07:08 -0400

    fix the footer to the bottom of pages for each individual generator

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-  max-width: 1240px;
-  margin: 0 auto;
-  padding: 40px;
+	min-height: calc(100vh - 668px);
+	max-width: 1240px;
+	margin: 0 auto;
+	padding: 40px;
 `
 
 export default Container
